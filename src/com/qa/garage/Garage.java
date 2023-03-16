3b52a8d7f366bb6302c1c3710688db13e0acc405
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-03-16 15:50:40 +0000

    Update Garage.java

--- src/com/qa/garage/Garage.java
+++ src/com/qa/garage/Garage.java
@@ -1,4 +1,5 @@
-package com.qa.garage;
+package com.qa.garage2;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,39 +9,131 @@ public class Garage {
 		// TODO Auto-generated method stub
 		//Vehicle v1 = new Vehicle("blue",4,"petrol","car");
 		//Vehicle v2 = new Vehicle("red",2,"petrol","");
-		Car c1 = new Car();
-		c1.setColour("blue");
-		c1.setEngineSize(1.1f);
-		c1.setFuelType("petrol");
-		c1.setNumberOfDoors(4);
-		c1.setNumberOfWheels(4);
-		c1.setVehicleType("car");
+		Vehicle v1 = new Car();
+		v1.setId(1);
+		v1.setColour("blue");
+		//v1.setEngineSize(1.1f);
+		v1.setFuelType("petrol");
+		//v1.setNumberOfDoors(4);
+		v1.setNumberOfWheels(4);
+		v1.setVehicleType("car");
 		
-		Lorry l1 = new Lorry();
-		l1.setColour("red");
-		l1.setFuelType("diesel");
-		l1.setNumberOfWheels(4);
-		l1.setVehicleType("lorry");
-		l1.setCubicCapacity(65);
-		l1.setHeight(5);
+		Vehicle v2 = new Lorry();
+		v2.setId(2);
+		v2.setColour("red");
+		v2.setFuelType("diesel");
+		v2.setNumberOfWheels(4);
+		v2.setVehicleType("lorry");
+		//v2.setCubicCapacity(65);
+		//v2.setHeight(5);
 		
-		Bus b1 = new Bus();
-		b1.setColour("white");
-		b1.setFuelType("diesel");
-		b1.setNumberOfWheels(4);
-		b1.setVehicleType("bus");
-		b1.setNumberOfFloors(2);
-		b1.setNumberOfSeats(50);
+		Vehicle v3 = new Bus();
+		v3.setId(3);
+		v3.setColour("white");
+		v3.setFuelType("diesel");
+		v3.setNumberOfWheels(4);
+		v3.setVehicleType("bus");
+		//v3.setNumberOfFloors(2);
+		//v3.setNumberOfSeats(50);
 		
 		List<Vehicle> vehicles = new ArrayList<>();
-		vehicles.add(c1);
-		vehicles.add(b1);
-		vehicles.add(l1);
+		vehicles.add(v1);
+		vehicles.add(v2);
+		vehicles.add(v3);
+		
+		
+		//calcBill(vehicles);		
+		Vehicle v4 = new Car(4,1.1f,4, "black", 4,"Petrol", "Car");
+		addVehicleToGarage(vehicles, v4);
+		
+		
+		//removeVehicleById(vehicles, 2);
+		
+		//emptyGarage(vehicles);
+
+		
+		List<Vehicle> vehiclesBeingRemoved= new ArrayList<>();
+		vehiclesBeingRemoved.add(v4);
+		vehiclesBeingRemoved.add(v1);
+
+		
+		removeMultipleVehicles (vehicles, vehiclesBeingRemoved);
+		
 		
 		for (Vehicle vehicle:vehicles) {
 			System.out.println(vehicle.getVehicleType());
 		}
 		
+	}
+	
+	public static void fixVehicle(Vehicle vehicle) {
+		float bill;
+		if (vehicle.getClass().getSimpleName().equals("Car")) {
+			bill = 100;
+			System.out.println("Vehicle is fixed and bill is "+bill+" pounds");			
+		}
+		else if (vehicle.getClass().getSimpleName().equals("Lorry")){
+			bill = 200;
+			System.out.println("Vehicle is fixed and bill is "+bill+" pounds");			
+		}			
+		else if (vehicle.getClass().getSimpleName().equals("Bus")){
+			bill = 300;
+			System.out.println("Vehicle is fixed and bill is "+bill+" pounds");			
+		}
+	}
+	
+	public static void removeMultipleVehicles (List<Vehicle> vehicles, List<Vehicle> vehiclesBeingRemoved) {
+		for (Vehicle v:vehiclesBeingRemoved){
+			for (Vehicle veh:vehicles) {
+				if (v.getId()==veh.getId()) {
+					vehicles.remove(v);
+					break;
+				}
+			}
+		}
+	}
+	
+	public static void emptyGarage(List<Vehicle> vehicles) {
+	
+		vehicles.removeAll(vehicles);
+	}
+	
+	public static void calcBill(List<Vehicle> vehicles) {
+		float bill=0;
+		for(Vehicle vehicle:vehicles)
+		{
+			if (vehicle.getClass().getSimpleName().equals("Car")) {
+				bill = 100;
+				System.out.println(bill);			
+			}
+			else if (vehicle.getClass().getSimpleName().equals("Lorry")){
+				bill = 200;
+				System.out.println(bill);			
+			}			
+			else if (vehicle.getClass().getSimpleName().equals("Bus")){
+				bill = 300;
+				System.out.println(bill);			
+			}
+		}
+	}
+	
+	public static void addVehicleToGarage(List<Vehicle> vehicles, Vehicle vehicle) {
+		for (Vehicle v:vehicles) {
+			if (v.getId()==vehicle.getId()) {
+				System.out.println("A vehicle of this id already exists");
+				return;
+			}
+		}
+		vehicles.add(vehicle);		
+	}
+	
+	public static void removeVehicleById(List<Vehicle> vehicles, int id) {
+		for (Vehicle vehicle:vehicles) {
+			if (vehicle.getId()==id) {
+				vehicles.remove(vehicle);
+				return;
+			}
+		}
 	}
 
 }
