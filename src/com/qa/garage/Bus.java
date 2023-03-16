5def6cdea6da8633fe0b1fe61767a6c44e14e68f
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-03-16 15:49:08 +0000

    Update Bus.java

--- src/com/qa/garage/Bus.java
+++ src/com/qa/garage/Bus.java
@@ -1,19 +1,21 @@
-package com.qa.garage;
+package com.qa.garage2;
 
-public class Bus extends Vehicle{
+public class Bus extends Vehicle {
 	
 	private int numberOfSeats;
 	private int numberOfFloors;
 	
-	public Bus() {
-		
-	}
-	
-	public Bus(int numberOfSeats, int numberOfFloors) {
+	public Bus(int numberOfSeats, int numberOfFloors,int id,String colour, int numberOfWheels, String fuelType, String vehicleType) {
+		super(id,colour, numberOfWheels, fuelType, vehicleType);
+		// TODO Auto-generated constructor stub
 		this.numberOfSeats = numberOfSeats;
 		this.numberOfFloors = numberOfFloors;
 	}
 
+	public Bus() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public int getNumberOfSeats() {
 		return numberOfSeats;
 	}
@@ -29,7 +31,6 @@ public class Bus extends Vehicle{
 	public void setNumberOfFloors(int numberOfFloors) {
 		this.numberOfFloors = numberOfFloors;
 	}
-	
 	
 
 }
